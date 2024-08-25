9965ec37e8c62cb6bb7bcc03bfc8d3c633219559
Sizen321 <Sizen321@mail.ru>
2024-08-25 22:26:26 +0300

    Added templates and changed views and urls

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+countries = open("country-by-languages.json", 'r')
+# iter_ = len(list(countries))
+# i = [i for i in range(1, iter_+1)]
+
+def home(request):
+    return render (request, "home_page.html", {})
+
+def countries_list(request):
+    context = {
+        "countries": countries.read()
+        }
+    return render(request, "countries_list.html", context)
